4883ab58a13054c1ec3414aedab285a74a470021
Anton Prince Oommen <antonprince95@gmail.com>
2025-01-06 16:13:41 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route path='/' index element={ <LoginForm />}/>
-=======
-        <Route path='/login' index element={ <LoginForm />}/>
->>>>>>> origin/main
         <Route path='/signup' element={<SignupForm/>}/>
         <Route path='/chat' element={<Chat/>} />
       </Routes>
