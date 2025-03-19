85964d199c233b4feab9c54e13383ca74bc1450c
Chithralekshmi-M-D <165647988+Chithralekshmi-M-D@users.noreply.github.com>
2025-03-19 19:02:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const mongoURL = process.env.mongo_url
 mongoose.connect(mongoURL).then(()=>{
     console.log("Database connected")
     app.listen(4000, ()=>{
-        console.log(`App is listening at 3000`)
+        console.log(App is listening at 3000)
     })
     }).catch((error)=>{
         console.log(error)
@@ -21,47 +21,50 @@ app.get('/login', async(req,res)=>{
     try {
         const {email,password} = req.body
         const result = await Users.findOne({email:email, password:password})
-        res.status(200).json(result)
+        if(!result)
+            return res.status(404).json({message:"user does not exist"})
+        else
+            return res.status(200).json(result)
     } catch (error) {
-        res.status(400).json({message:"User not found"})
+        return res.status(400).json({message:"User not found"})
     }
  
 })
 
-//user register
+#user register
 app.post('/create_user', async(req, res)=>{
     try {
         const user = req.body
         await Users.create(user)
-        res.status(200).json({message:"Successfully created"})
+        return res.status(200).json({message:"Successfully created"})
     } catch (error) {
-        res.status(400).json(error)
+        return res.status(400).json(error)
     }
 })
 
-//searching legal professionals for users
+#searching legal professionals for users
 app.get('/search', async(req, res)=>{
     try {
         const {special} = req.body
         const result = await Users.find({specialization:special})
         return res.status(200).json(result)
     } catch (error) {
-        res.status(400).json({message:"None exists"})
+        return res.status(400).json({message:"None exists"})
     }
 })
 
-//to get information about user
+#to get information about user
 app.get('/get_user', async(req,res)=>{
     try {
          const {email} = req.body
          const result = await Users.findOne({email:email})
-         res.status(200).json(result)
+         return res.status(200).json(result)
     } catch (error) {
         return res.status(400).json({message:"Error occured try again"})
     }
 })
 
-//dont use this now
+#dont use this now
 app.put('/update_history', async (req, res) => {
     const { email, index, newContent } = req.body;
 
@@ -89,10 +92,10 @@ app.put('/update_history', async (req, res) => {
             return res.status(200).json({ message: "History added successfully", user });
         }
 
-        res.status(200).json({ message: "Content updated successfully", updatedUser });
+        return res.status(200).json({ message: "Content updated successfully", updatedUser });
 
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Server error", error });
+        return res.status(500).json({ message: "Server error", error });
     }
 });
